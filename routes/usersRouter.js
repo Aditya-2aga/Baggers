aee9c833ef4f19bd7a10d04828c212c47c1959e8
Aditya-2aga <aditya.2aga@gmail.com>
2024-11-10 23:48:38 +0530

    Only cart left

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {registerUser, loginUser, logout} = require("../controllers/authController")
-
+ 
 router.get("/", function(req, res){
     res.send("Hey")
 })
